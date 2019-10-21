0fe5026ad56e5a450a15e8d6731c6e01cfdaf117
mateus lima de matos <mateuslima565@gmail.com>
2019-10-21 18:27:06 -0300

    ajuste no path do Json

--- API ARDUINO/controller/FluxoDagua.js
+++ API ARDUINO/controller/FluxoDagua.js
@@ -2,7 +2,7 @@ const obterDadosArquivo = require('../utils')
 
 module.exports = {
     async fluxo(req, res) {
-        var objectJason = await obterDadosArquivo('./fluxo.json');
+        var objectJason = await obterDadosArquivo('../espWebServer/data/config.json');
         // console.log(objectJason)
         return res.send(objectJason)
     }
